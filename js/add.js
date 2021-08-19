ad77a50374e3c4240cda3b005280df1240075e8a
volodymyr2222 <volody9089@gmail.com>
2021-08-19 22:52:40 +0300

    first commt

--- js/add.js
+++ js/add.js
@@ -11,21 +11,36 @@ formsButton.addEventListener('click', function () {
   const block = document.querySelectorAll('.main_block .block_forms');
   const input = document.querySelectorAll('.main_block .forms_time');
    // провірка на заповнення імпутів
+
   let bull = true
   input.forEach((item) => {
       if (item.children[1].value == '') {
           bull = false
+          // перевірка на інпути
+          item.children[2].style.opacity = 1
       }
      })
 
+//додавання блоку
+      if(block.length < 7 && bull && erororChe() === 0) {
+      appendBlock()  
+    } else {
+      erororChe()
+    }
+   auditCheckbox ()
 
-     //провірка на заповення чекбоксів
+});
+ //провірка на заповення чекбоксів
+function erororChe() {
+  const block = document.querySelectorAll('.main_block .block_forms');
      let arrCheckboxBool = [];
      for(let key of block) {
        let res = key.children[4].children
        let arrChek = []
+       let arr_chekbox
        for(let i = 0; i < res.length-1;i++) {
         const resBull = res[i].children[1].checked
+        arr_chekbox = res[i].parentElement.parentElement;
         if(resBull === true) {
           arrCheckboxBool.push(resBull)
         } else {
@@ -33,29 +48,24 @@ formsButton.addEventListener('click', function () {
         }
      }
      if(arrChek.length === 7){
+      arr_chekbox.children[3].children[3].style.opacity = 1
       arrCheckboxBool.push(false)
-     }
+
+     } 
     }
 let filterCheckbox = arrCheckboxBool.filter(i => i === false).length
+return filterCheckbox
+}
 
-//додавання блоку
-       if(block.length < 7 && bull && filterCheckbox === 0) {
-      appendBlock()  
-    } else {
-      alert('помилка')
-    }
 
-   auditCheckbox ()
-});
-
+// клонування блоку
 function appendBlock() {
   const a =  blockForms.cloneNode(true); 
   a.classList.remove('pers')
   mainBlock.append(a);
 
   auditInput(); 
-  
- removeBlock();
+  removeBlock();
 }
 appendBlock()
 
@@ -76,11 +86,10 @@ function auditInput () {
   let bArr = b.slice(0,2).join('')
 
     if (b != '') {
-    
       if (aArr < bArr) {
     } else {
-              this.value=''
-        alert('помилка')
+        this.value=''
+        this.parentElement.children[2].style.opacity = 1;
     }
 }
   }
@@ -89,117 +98,98 @@ function auditInput () {
 
   if(value.length < 6 ) {
   if (value.length === 1 && parseInt(+valueArr[0])) {
+
         this.value = `0${value}:00`.split();
-
-  } else if(value.length === 1 && !parseInt(+valueArr[0])){
+  } else if(!parseInt(+valueArr[0])){
     this.value=''
-    alert('помилка');
+    // this.parentElement.children[2].innerHTML = 'ERROR ENTER NUMBERS'
+    this.parentElement.children[2].style.opacity = 1;
   }
-
   if (value.length === 2 && parseInt(+valueArr[1])) {
     if(+valueArr[0] < 3){
       if (+valueArr[0] == 2) {
         if (+valueArr[1] > 3) {
       this.value=''
-      alert('помилка')
+      this.parentElement.children[2].style.opacity = 1;
         }else {
         this.value = `${value[0]}${value[1]}:00`.split();
-    
       }
       } else {
   this.value = `${value[0]}${value[1]}:00`.split();
-
           }
-
     } else{
       this.value=''
-      alert('помилка')
+      this.parentElement.children[2].style.opacity = 1;
     }
    } else if(value.length === 2 && !parseInt(+valueArr[1])) {
     this.value=''
-    alert('помилка')
+    this.parentElement.children[2].style.opacity = 1;
    }
-   
    if (value.length === 3 && parseInt(+valueArr[1])) { 
-   
     if(+valueArr[0] < 3 && +valueArr[2] < 6){
       if (+valueArr[0] == 2 ) {
         if (+valueArr[1] > 3 ) {
       this.value=''
-      alert('помилка')
+      this.parentElement.children[2].style.opacity = 1;
         }else {
    this.value = `${value[0]}${value[1]}:${value[2]}0`.split();
-
       }
       } else {
         this.value = `${value[0]}${value[1]}:${value[2]}0`.split();
-   
           }
-
     } else{
       this.value=''
-      alert('помилка')
+      this.parentElement.children[2].style.opacity = 1;
     }
-
    }else if (value.length === 3 && !parseInt(+valueArr[1])) {
     this.value=''
-    alert('помилка')
+    this.parentElement.children[2].style.opacity = 1;
    }
-
    if (value.length === 4 && parseInt(+valueArr[1]) && parseInt(+valueArr[1])) {
     if(+valueArr[0] < 3 && +valueArr[2] < 6){
       if (+valueArr[0] == 2 ) {
         if (+valueArr[1] > 3 ) {
       this.value=''
-      alert('помилка')
+      this.parentElement.children[2].style.opacity = 1;
         }else {
   this.value = `${value[0]}${value[1]}:${value[2]}${value[3]}`.split();
-
       }
       } else {
         this.value = `${value[0]}${value[1]}:${value[2]}${value[3]}`.split();
-        
           }
-
     } else{
       this.value=''
-      alert('помилка')
+      this.parentElement.children[2].style.opacity = 1;
     }
    } else if (value.length === 4 && !parseInt(+valueArr[1]) &&!parseInt(+valueArr[3])) {
     this.value=''
-    alert('помилка')
+    this.parentElement.children[2].style.opacity = 1;
    }
-
   if (value.length === 5 ) {
     if(value[2] == ':' && +valueArr[0] < 3 && +valueArr[3] < 6){
       if (+valueArr[0] == 2 ) {
         if (+valueArr[1] > 3 ) {
       this.value=''
-      alert('помилка')
+      this.parentElement.children[2].style.opacity = 1;
         this.value = `${value[0]}${value[1]}${value[2]}${value[3]}${value[4]}`.split();
-
       }
       } else {
    this.value = `${value[0]}${value[1]}${value[2]}${value[3]}${value[4]}`.split();
-
           }
-
     } else{
       this.value=''
-      alert('помилка')
+      this.parentElement.children[2].style.opacity = 1;
     }
    }
-
   }else {
     this.value=''
-    alert('помилка')
+    this.parentElement.children[2].style.opacity = 1;
   }
-
   });
 key.addEventListener('focus', function () {
   this.value=''
+  this.parentElement.children[2].style.opacity = 0;
   })
-
   }
 }
 
@@ -209,26 +199,30 @@ function auditCheckbox (){
   for(const item of block){
     item.children[2].addEventListener('click', function (){
       let element = item.children[0].textContent.trim()
+      this.parentElement.parentElement.parentElement.children[3].children[3].style.opacity = 0
       for (const i of checkboxRem){
        let a = i.children[0].textContent.trim()
        
         if (element === a) {
           i.children[1].disabled = true
+          i.children[2].classList.add('allCheckmark1')
         } 
           }
       if (item.children[1].checked) {
         item.children[1].disabled = false
-
             for(let i of block){
               let a = i.children[0].textContent.trim()
               if (element == a) {
                 i.children[1].disabled = false
+                i.children[2].classList.remove('allCheckmark1')
               }
               }
             for(let i of checkboxRem){
               let a = i.children[0].textContent.trim()
+              
               if (element == a) {
                 i.children[1].disabled = false
+                
               }
             }
           } else {
@@ -237,7 +231,10 @@ function auditCheckbox (){
                 let a = i.children[0].textContent.trim()
                 if(element === a){
                   i.children[1].disabled = true
-
+                  if (i.children[1].checked == false){
+                    
+                    i.children[2].classList.add('allCheckmark1')
+                  }
                 }
               }
             })
@@ -261,13 +258,14 @@ function info() {
       var input_1 = key.children[1].children[1].value
       var input_2 = key.children[3].children[1].value
       const checkbox = key.children[4].children
-        
-
       var checkboxArr = []
+// console.log(key);
       for (let key of checkbox) {
+         console.log(key);
         if(key.children[1].checked){
           let a = key.children[0].textContent
           checkboxArr.push(a)
+         
         }
       }
       const infoObj = {
@@ -275,14 +273,18 @@ function info() {
             time1: input_1,
             time2: input_2,
         },
-
     days: checkboxArr
   }
   infoArr.push(infoObj);  
 }
 
 if (input_1 !== '' && input_2 !== '' && checkboxArr.length) {
-  console.log(infoArr);
+  // console.log(infoArr);
+} else if (checkboxArr.length === 0) {
+  for(let key of info) {
+key.children[3].children[3].style.opacity = 1
+  }
+  
 }
 
 
@@ -310,7 +312,6 @@ const block2 = document.querySelectorAll('main .main_block .block_forms .checkbo
 let a = item.path[1].children[4].children
 
 for(let element of a){
-console.log(element.children[1].checked);
   if (element.children[1].checked) {
     let span = element.children[0].textContent.trim()
     for (let i of block1) {
@@ -323,7 +324,8 @@ console.log(element.children[1].checked);
         let span1 = i.children[0].textContent.trim()
         if (span === span1) {
           i.children[1].disabled = false
-       
+          i.children[2].classList.remove('allCheckmark1')
+          
           }
         }
 }
